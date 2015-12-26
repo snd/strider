e84ed6d296b86b059bd6bcb5b1f3ca5e83498d22
Maximilian Krüger <kruemaxi@googlemail.com>
2015-12-26 21:19:02 +0100

    improve test naming

--- tests/lib.rs
+++ tests/lib.rs
@@ -6,12 +6,12 @@ use sliding_window::SliceRing;
 use sliding_window::OptimizedSliceRing;
 
 #[test]
-fn test_deque_slice_ring() {
+fn test_slice_ring_unoptimized() {
     test_slice_ring!(VecDeque::<i32>::new());
 }
 
 #[test]
-fn test_optimized_slice_ring() {
+fn test_slice_ring_optimized() {
     test_slice_ring!(OptimizedSliceRing::<i32>::new());
 }
 // #[test]
