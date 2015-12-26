dbf5c233c642369766b5800eb81c2db3ac567587
Maximilian Krüger <kruemaxi@googlemail.com>
2015-12-26 21:16:09 +0100

    use macros to remove duplication from benches

--- benches/empty_to_probe_for_release_mode.rs
+++
@@ -1,7 +0,0 @@
-#![feature(test)]
-extern crate test;
-
-#[bench]
-fn bench_empty_to_probe_for_release_mode(b: &mut test::Bencher) {
-    b.iter(|| 1)
-}
---
+++ benches/lib.rs
@@ -0,0 +1,86 @@
+#![feature(test)]
+extern crate test;
+
+use std::collections::VecDeque;
+
+#[macro_use]
+extern crate sliding_window;
+use sliding_window::SliceRing;
+use sliding_window::OptimizedSliceRing;
+
+#[bench]
+fn bench_empty_to_probe_for_release_mode(b: &mut test::Bencher) {
+    b.iter(|| 1)
+}
+
+macro_rules! bench_push_many_back {
+    ($bencher:expr, $new:expr) => {{
+        let vec = (0..1000).collect::<Vec<i32>>();
+        $bencher.iter(|| {
+            let mut testable = $new;
+            testable.push_many_back(&vec[..]);
+            testable
+        });
+    }}
+}
+#[bench]
+fn bench_push_many_back_unoptimized(b: &mut test::Bencher) {
+    bench_push_many_back!(b, VecDeque::new());
+}
+#[bench]
+fn bench_push_many_back_optimized(b: &mut test::Bencher) {
+    bench_push_many_back!(b, OptimizedSliceRing::new());
+}
+
+macro_rules! bench_push_many_back_drop_many_front {
+    ($bencher:expr, $new:expr) => {{
+        let vec = (0..1000).collect::<Vec<i32>>();
+        $bencher.iter(|| {
+            let mut testable = $new;
+            testable.push_many_back(&vec[..]);
+            testable.drop_many_front(1000)
+        });
+    }}
+}
+
+#[bench]
+fn bench_push_many_back_drop_many_front_unoptimized(b: &mut test::Bencher) {
+    bench_push_many_back!(b, VecDeque::new());
+}
+#[bench]
+fn bench_push_many_back_drop_many_front_optimized(b: &mut test::Bencher) {
+    bench_push_many_back!(b, OptimizedSliceRing::new());
+}
+
+macro_rules! bench_read_many_front {
+    ($bencher:expr, $new:expr) => {{
+        let mut output: Vec<i32> = std::iter::repeat(0).take(1000).collect();
+        let vec = (0..1000).collect::<Vec<i32>>();
+        let mut testable = $new;
+        testable.push_many_back(&vec[..]);
+        $bencher.iter(|| {
+            testable.read_many_front(&mut output[..])
+        });
+    }}
+}
+#[bench]
+fn bench_read_many_front_unoptimized(b: &mut test::Bencher) {
+    bench_read_many_front!(b, VecDeque::new());
+}
+#[bench]
+fn bench_read_many_front_optimized(b: &mut test::Bencher) {
+    bench_read_many_front!(b, OptimizedSliceRing::new());
+}
+
+#[bench]
+fn bench_test_slice_ring_unoptimized(b: &mut test::Bencher) {
+    b.iter(|| {
+        test_slice_ring!(VecDeque::<i32>::new());
+    });
+}
+#[bench]
+fn bench_test_slice_ring_optimized(b: &mut test::Bencher) {
+    b.iter(|| {
+        test_slice_ring!(OptimizedSliceRing::<i32>::new())
+    });
+}
